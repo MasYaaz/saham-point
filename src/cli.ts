4ae20f35a3c3a539d3b10f3a60c57a594d9d09c7
Aflah <diyaz.hal22@gmail.com>
2026-07-22 16:20:41 +0700

    Fix : Pemisahan responsibility service utama dengan cli

---
+++ src/cli.ts
@@ -0,0 +1,31 @@
+import { handleCommand } from "./cli/command";
+import { readlineHead } from "./cli/component/readlineInterface";
+import {
+  acceptSuggestion,
+  clearSuggestion,
+  renderGhostSuggestion,
+} from "./cli/helper/autoCompletion";
+import { TUIHead } from "./cli/component/TUIHead";
+
+// Render TUI Awal
+TUIHead();
+readlineHead.prompt();
+
+// Interaksi Keyboard
+process.stdin.on("keypress", (str, key) => {
+  if (key.name === "right" || key.name === "tab") {
+    if (acceptSuggestion()) return;
+  }
+  renderGhostSuggestion();
+});
+
+// Listener Input Command
+readlineHead.on("line", async (line) => {
+  clearSuggestion();
+  await handleCommand(line, TUIHead);
+});
+
+readlineHead.on("SIGINT", () => {
+  console.log("\n👋 Mematikan TUI Terminal...");
+  process.exit(0);
+});
